1ca1b9d3ce21eb49cf4dc2920f6e7eb3e51cd652
aashish-big-data <aashish.raina@ymail.com>
2020-11-26 20:10:36 +0530

    Create program.py

---
+++ PAYROLL_SYSTEM/program.py
@@ -0,0 +1,23 @@
+import hr
+import employees
+import productivity
+
+manager = employees.Manager(1, 'John Smith', 1500)
+secretary = employees.Secretary(2, 'Jane Doe', 1200)
+sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
+factory_worker = employees.FactoryWorker(4, 'Pete Peterson', 40, 15)
+temporary_secretary = employees.TemporarySecretary(5, 'Robin Williams', 40, 9)
+
+employees = [
+    manager,
+    secretary,
+    sales_guy,
+    factory_worker,
+    temporary_secretary
+]
+
+productivity_system = productivity.ProductivitySystem()
+productivity_system.track(employees, 40)
+
+payroll_system = hr.PayrollSystem()
+payroll_system.calculate_payroll(employees)
